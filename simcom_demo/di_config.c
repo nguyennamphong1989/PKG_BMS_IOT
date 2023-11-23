21105030af2978541fee231fae04ed008a6ed0b6
Administrator <thuckumin@gmail.com>
2023-11-23 17:04:00 +0700

    5.03pm version 1.0.0

--- simcom_demo/di_config.c
+++ simcom_demo/di_config.c
@@ -199,16 +199,16 @@ void uart_read()
     }
     //print test
     // memset(temp, 0, 50);
-    // sprintf(temp, "\n Queue-nbMsg = %d", nbMsg);
-    // sAPI_UartWrite(SC_UART3, temp,strlen(temp));
+    sprintf(temp, "\n Queue-nbMsg = %d", nbMsg);
+    sAPI_UartWrite(SC_UART3, temp,strlen(temp));
 
     sAPI_UartWrite(SC_UART, request, 21);
     sAPI_TaskSleep(200);
     sAPI_MsgQPoll(simcomUI_msgq, &nbMsg);
     // print test
-    // memset(temp, 0, 50);
-    // sprintf(temp, "\n Readall nbMsg = %d", nbMsg);
-    // sAPI_UartWrite(SC_UART3, temp,strlen(temp));
+    memset(temp, 0, 50);
+    sprintf(temp, "\n Readall nbMsg = %d", nbMsg);
+    sAPI_UartWrite(SC_UART3, temp,strlen(temp));
 
     while (nbMsg > 0)
     {
@@ -725,7 +725,7 @@ void uart_read()
 
     if(sAPI_GetTicks()>BMS_tick) //read BMS and send update to server every 1 min
     {
-        if(sAPI_GetTicks()-BMS_tick>MQTT_SEND_TIME || BMS_tick< (200*5) ) //1 mins
+        if(sAPI_GetTicks()-BMS_tick>MQTT_SEND_TIME || BMS_tick< (200*30) ) //1 mins
         {
             Publish_array(payload);
             BMS_tick=sAPI_GetTicks();
